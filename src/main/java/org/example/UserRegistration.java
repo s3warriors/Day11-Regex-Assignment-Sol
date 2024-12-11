0f3ad81abf48c7ff1147be49443025e066d6f99f
s3warriors <satveerswami982@gmail.com>
2024-12-11 12:20:26 +0530

    Added email samples for validation testing purposes

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRegistration {
 
     // Method to validate first name
@@ -37,7 +40,14 @@ public class UserRegistration {
                 password.matches(rule3) &&
                 password.matches(rule4);
     }
-
+    // UC9: Method to validate a list of emails
+    public List<Boolean> validateEmailSamples(List<String> emails) {
+        List<Boolean> results = new ArrayList<>();
+        for (String email : emails) {
+            results.add(validateEmail(email));
+        }
+        return results;
+    }
     public static void main(String[] args) {
         UserRegistration userRegistration = new UserRegistration();
 
@@ -62,6 +72,37 @@ public class UserRegistration {
         System.out.println("Password Validation:");
         System.out.println(userRegistration.validatePassword("Abc@1234")); // Should return true
         System.out.println(userRegistration.validatePassword("abc1234")); // Should return false
+
+        List<String> emailSamples = List.of(
+                // Valid Emails
+                "abc@yahoo.com",         // Valid
+                "abc-100@yahoo.com",     // Valid
+                "abc.100@yahoo.com",     // Valid
+                "abc111@abc.com",        // Valid
+                "abc-100@abc.net",       // Valid
+                "abc.100@abc.com.au",    // Valid
+                "abc@1.com",             // Valid
+                "abc@gmail.com.com",     // Valid
+                "abc+100@gmail.com",     // Valid
+
+                // Invalid Emails
+                "abc",                   // Invalid - Missing "@" symbol
+                "abc@.com.my",           // Invalid - TLD starts with a dot
+                "abc123@gmail.a",        // Invalid - TLD must have at least two characters
+                "abc123@.com",           // Invalid - TLD starts with a dot
+                "abc123@.com.com",       // Invalid - TLD starts with a dot
+                ".abc@abc.com",          // Invalid - First character is a dot
+                "abc()*@gmail.com",      // Invalid - Contains invalid characters
+                "abc@%*.com",            // Invalid - TLD contains invalid characters
+                "abc..2002@gmail.com",   // Invalid - Contains double dots
+                "abc.@gmail.com",        // Invalid - Ends with a dot
+                "abc@abc@gmail.com",     // Invalid - Contains multiple "@" symbols
+                "abc@gmail.com.1a",      // Invalid - TLD with two characters contains a digit
+                "abc@gmail.com.aa.au"    // Invalid - Multiple TLDs are not allowed
+        );
+
+        System.out.println("Email Samples Validation:");
+        System.out.println(userRegistration.validateEmailSamples(emailSamples));
 
     }
 }
