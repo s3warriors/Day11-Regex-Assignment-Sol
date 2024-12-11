2421c0cec3ee1c71b7cc5199a773eddffdf8c032
s3warriors <satveerswami982@gmail.com>
2024-12-11 11:56:26 +0530

    Added validation for Email

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -14,6 +14,12 @@ public class UserRegistration {
         return lastName.matches(regex);
     }
 
+    // Method to validate email (UC3)
+    public boolean validateEmail(String email) {
+        String regex = "^[a-zA-Z0-9]+([._+-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$"; // Valid email regex
+        return email.matches(regex);
+    }
+
     public static void main(String[] args) {
         UserRegistration userRegistration = new UserRegistration();
 
@@ -25,5 +31,9 @@ public class UserRegistration {
         System.out.println("Last Name Validation:");
         System.out.println(userRegistration.validateLastName("Doe"));  // Should return true
         System.out.println(userRegistration.validateLastName("do"));   // Should return false
+
+        System.out.println("Email Validation:");
+        System.out.println(userRegistration.validateEmail("abc.xyz@bl.co.in")); // Should return true
+        System.out.println(userRegistration.validateEmail("abc@.com.my"));     // Should return false
     }
 }
